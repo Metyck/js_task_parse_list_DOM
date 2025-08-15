e7b7477bcb98f998d61c26f08ead310fee6374e9
Matvey Turluk <matychencia.ua@gmail.com>
2025-08-15 13:14:43 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+// first function
+function sortList(workers) {
+  workers.sort((a, b) => {
+    const salaryA = parseInt(
+      a.getAttribute('data-salary').replace(/[^0-9]/g, ''),
+    );
+    const salaryB = parseInt(
+      b.getAttribute('data-salary').replace(/[^0-9]/g, ''),
+    );
+
+    return salaryB - salaryA;
+  });
+
+  const list = document.querySelector('ul');
+
+  workers.forEach((worker) => list.appendChild(worker));
+
+  return workers;
+}
+
+// second function
+const employees = Array.from(document.querySelectorAll('ul li'));
+
+sortList(employees);
+
+function getEmployees(employs) {
+  return employs.map((employee) => ({
+    name: employee.textContent.trim(),
+    position: employee.getAttribute('data-position'),
+    salary: employee.getAttribute('data-salary'),
+    age: employee.getAttribute('data-age'),
+  }));
+}
+
+getEmployees(employees);
